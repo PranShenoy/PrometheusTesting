fcd6d1dd9830a391747fb9b13b70ff08e66a0e12
Pranati Nayak <FRL@kmd.dk>
2023-01-17 15:21:30 +0530

    First Commit

---
+++ src/main/java/com/nynemkonto/PrometheusTestingApplication.java
@@ -0,0 +1,13 @@
+package com.nynemkonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nynemkonto/PrometheusTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nynemkonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
